bac105597fd9ea15fcc05f7fd18f40741364e70f
rabindraverma <109429369+rabindraverma@users.noreply.github.com>
2023-02-03 17:56:34 +0530

    Update data.sql

--- data.sql
+++ data.sql
@@ -54,39 +54,13 @@ FOREIGN KEY(FLIGHT_ID) REFERENCES FLIGHT(ID)
 ) 
 SELECT * FROM RESERVATION
 
-insert into flight values(
-1,
-'AA1',
-'American Airlines',
-'AUS',
-'NYC',
-str_to_date('02-05-2018','%d-%m-%Y'),
-'2018-02-05 03:14:07'
-)
-insert into flight values(
-2,'AA2','American Airlines','AUS','NYC',
-str_to_date('02-05-2018','%d-%m-%Y'),'2018-02-05 05:14:07')
-insert into flight values(
-3,'AA3','American Airlines','AUS','NYC',
-str_to_date('02-05-2018','%d-%m-%Y'),'2018-02-05 06:14:07')
-insert into flight values(
-4,'SW1','South West','AUS','NYC',
-str_to_date('02-05-2018','%d-%m-%Y'),'2018-02-05 07:14:07')
-insert into flight values(
-5,'UA1','United Airlines','NYC','DAL',
-str_to_date('02-05-2018','%d-%m-%Y'),'2018-02-05 10:14:07')
-insert into flight values(
-6,'UA1','United Airlines','NYC','DAL',
-str_to_date('02-05-2018','%d-%m-%Y'),'2018-02-05 10:14:07')
-insert into flight values(
-7,'SW1','South West','AUS','NYC',
-str_to_date('02-06-2018','%d-%m-%Y'),'2018-02-06 07:14:07')
-insert into flight values(
-8,'SW2','South West','AUS','NYC',
-str_to_date('02-06-2018','%d-%m-%Y'),'2018-02-06 08:14:07')
-insert into flight values(
-9,'SW3','South West','NYC','DAL',
-str_to_date('02-06-2018','%d-%m-%Y'),'2018-02-06 10:14:07')
-insert into flight values(
-10,'UA1','United Airlines','NYC','DAL',
-str_to_date('02-06-2018','%d-%m-%Y'),'2018-02-06 10:14:07')
+insert into flight values(1,'AA1','American Airlines','AUS','NYC',str_to_date('02-05-2018','%d-%m-%Y'),'2018-02-05 03:14:07')
+insert into flight values(2,'AA2','American Airlines','AUS','NYC',str_to_date('02-05-2018','%d-%m-%Y'),'2018-02-05 05:14:07')
+insert into flight values(3,'AA3','American Airlines','AUS','NYC',str_to_date('02-05-2018','%d-%m-%Y'),'2018-02-05 06:14:07')
+insert into flight values(4,'SW1','South West','AUS','NYC',str_to_date('02-05-2018','%d-%m-%Y'),'2018-02-05 07:14:07')
+insert into flight values(5,'UA1','United Airlines','NYC','DAL',str_to_date('02-05-2018','%d-%m-%Y'),'2018-02-05 10:14:07')
+insert into flight values(6,'UA1','United Airlines','NYC','DAL',str_to_date('02-05-2018','%d-%m-%Y'),'2018-02-05 10:14:07')
+insert into flight values(7,'SW1','South West','AUS','NYC',str_to_date('02-06-2018','%d-%m-%Y'),'2018-02-06 07:14:07')
+insert into flight values(8,'SW2','South West','AUS','NYC',str_to_date('02-06-2018','%d-%m-%Y'),'2018-02-06 08:14:07')
+insert into flight values(9,'SW3','South West','NYC','DAL',str_to_date('02-06-2018','%d-%m-%Y'),'2018-02-06 10:14:07')
+insert into flight values(10,'UA1','United Airlines','NYC','DAL',str_to_date('02-06-2018','%d-%m-%Y'),'2018-02-06 10:14:07')
